dfb37a5b2face5c66fc1f6e4f058d9b81b8e68c8
AodHaxan <hsn.aouidy@gmail.com>
2023-04-27 09:23:28 -0700

    singly linked lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,20 +11,34 @@
 */
 list_t *add_node(list_t **head, const char *str)
 {
+	if (head == NULL)
+	{
+	return (NULL);
+	}
+
 	list_t *new;
 	unsigned int len = 0;
 
 	while (str[len])
-	len++; 2
+	{
+	len++;
+	}
 
 	new = malloc(sizeof(list_t));
 	if (!new)
+	{
 	return (NULL);
-
+	}
 	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+	free(new);
+	return (NULL);
+	}
+
 	new->len = len;
 	new->next = (*head);
 	(*head) = new;
 
-	return (*head);
+	return (new);
 }
