f793bb69e15d8b2ec212f57fbf43a3abcf51a23e
AodHaxan <126115729+AodHaxan@users.noreply.github.com>
2023-05-04 01:48:26 +0200

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+size_t looped_listint_len(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
+
 /**
  * print_listint_safe - Prints a listint_t linked list.
  * @head: A pointer to the head of the listint_t list.
